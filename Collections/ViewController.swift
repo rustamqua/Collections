5d8b00f900c047ae3af56bab41eaed848ee70588
Rustam-Deniz Emirali <emirali.rustam.deniz@gmail.com>
2020-07-30 16:21:02 +0600

    Initial Commit

---
+++ Collections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by Rustam-Deniz on 7/30/20.
+//  Copyright © 2020 Rustam-Deniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
